07bcbd8649f0a2bf195a8e3d9fc12bc749b7ac55
Violet Mehrzad <viomehr@gmail.com>
2024-01-31 12:18:11 +0330

    Server / Initialize Mongoose - dotenv - appRootPath

--- Server/index.js
+++ Server/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const appRoot = require('app-root-path');
+require('dotenv').config({
+    path: appRoot + '/.env'
+})
 
+const app = express();
 app.use(cors());
 app.use(express.json());
+mongoose.connect(process.env.DB_URI)
 
 
 const PORT = process.env.PORT || 5000;
